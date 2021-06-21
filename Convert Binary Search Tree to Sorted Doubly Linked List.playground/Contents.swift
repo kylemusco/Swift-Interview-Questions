c57ad3d80d57f62c4de6c1d5d024d885fb197bdd
Kyle Musco <kmusco@ycp.edu>
2021-06-20 20:44:59 -0600

    426: Convert Binary Search Tree to Sorted Doubly Linked List
    https://leetcode.com/problems/convert-binary-search-tree-to-sorted-doubly-linked-list/

---
+++ Convert Binary Search Tree to Sorted Doubly Linked List.playground/Contents.swift
@@ -0,0 +1,50 @@
+import Foundation
+
+public class Node {
+    public var val: Int
+    public var left: Node?
+    public var right: Node?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
+
+class Solution {
+    var first: Node? = nil
+    var last: Node? = nil
+    
+    func treeToDoublyList(_ root: Node?) -> Node? {
+        if root == nil {
+            return nil
+        }
+        
+        helper(root)
+        
+        last!.right = first
+        first!.left = last
+        
+        return first
+    }
+    
+    private func helper(_ node: Node?) {
+        guard node != nil else {
+            return
+        }
+        
+        helper(node!.left)
+        
+        if last != nil {
+            // Link the previous node (last) with the current one (node)
+            last!.right = node
+            node!.left = last
+        } else {
+            // Keep the smallest node to close DLL later on
+            first = node
+        }
+        
+        last = node
+        helper(node!.right)
+    }
+}
